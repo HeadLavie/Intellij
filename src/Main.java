23d9781e7e9da999680fd3deb6f6ba6f24430439
lavie head <nadya.karpenko@gmail.com>
2023-07-28 22:21:30 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2 + 3");
+        System.out.println(2+3);
+
+        int speed;
+        speed = 17;
+
+        System.out.println(speed);
+
+        int x = speed + 3;
+
+        double d = 6.5;
+
+        String name = "Nadya";
+
+        System.out.println(name + " ехал со скоростью " + speed);
+    }
+
+}
